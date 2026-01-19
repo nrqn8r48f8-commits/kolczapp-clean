91faf84b106a153e7e864360a2186438d717c9df
Tomasz Florek <kolcz@Mac-mini-Tomasz.local>
2026-01-19 23:38:19 +0100

    Deploy test

--- app/page.js
+++ app/page.js
@@ -1,10 +1,3 @@
 export default function Home() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>KOŁCZ-APP</h1>
-      <p style={{ color: 'red', fontWeight: 'bold' }}>
-        ONLINE TEST — JEŚLI TO WIDZISZ, DEPLOY DZIAŁA
-      </p>
-    </main>
-  )
+  return <h1 style={{color: 'red'}}>DEPLOY TEST 123</h1>
 }
